a8d643b956e13345c41dd4fea5bc663628bfb837
jwwalls <jwwalls@users.noreply.github.com>
2023-07-18 15:08:10 -0500

    navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,24 @@
 import "./navbar.scss"
 const Navbar = () => {
     return (
-        <div className="navbar">Navbar 
+        <div className="navbar">
             <div className="logo">
-                <img src="" alt="" />
+                <img src="logo.svg" alt="" />
+                <span>Admin</span>
+            </div>
+            <div className="icons">
+                <img src="/search.svg" alt="" />
+                <img src="/app.svg" alt="" />
+                <img src="/expand.svg" alt="" />
+                <div className="notification">
+                    <img src="/notifications.svg" alt="" />
+                    <span>1</span>
+                </div>
+                <div className="user">
+                    <img src="/selfie.jpg" alt="" />
+                    <span >Jeremie</span>
+                </div>
+                <img src="/settings.svg" alt="" />
             </div>
 
         </div>
