9c3543d8702fc80855991d9fa54ba7f601e01929
jwwalls <jwwalls@users.noreply.github.com>
2023-07-18 14:50:21 -0500

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,57 @@
-function App() {
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import "./styles/global.scss"
 
-  return (
-    <div>Hello World</div>
-  )
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer"> 
+            <Outlet/>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "users",
+          element: <Users />,
+        },
+        {
+          path: "products",
+          element: <Products />,
+        },
+      ]
+    },
+   
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import "./navbar.scss"
+const Navbar = () => {
+    return (
+        <div className="navbar">Navbar 
+            <div className="logo">
+                <img src="" alt="" />
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
